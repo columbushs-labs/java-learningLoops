3dfc53107818dbf818c1e82c2af704a2d46e170f
Matthew Larrubia <mlarrubia@columbushs.com>
2019-11-04 13:24:52 -0500

    Inital Commit

---
+++ src/java/learningloops/JavaLearningLoops.java
@@ -0,0 +1,82 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java.learningloops;
+
+/**
+ *
+ * @author Matthew
+ */
+public class JavaLearningLoops {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        // Happy Coding!
+        
+//        question1();
+//        question2();
+//        question3();
+//        question4();
+//        question5();
+//        question6();
+//        question7();
+//        question8();
+//        question9();
+//        question10();
+//        question11();
+        
+    }
+    
+    public static void question1(){
+        
+    }
+    
+    public static void question2(){
+        
+    }
+    
+    public static void question3(){
+        
+    }
+    
+    public static void question4(){
+        
+    }
+    
+    public static void question5(){
+        
+    }
+    
+    public static void question6(){
+        
+    }
+    
+    public static void question7(){
+        
+    }
+    
+    public static void question8(){
+        
+    }
+    
+    public static void question9(){
+        
+    }
+    
+    public static void question10(){
+        
+    }
+    
+    public static void question11(){
+        
+    }
+    
+    
+    
+    
+}
